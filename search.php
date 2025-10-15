2f667a27bc9f76d1dbb5b3432aa8e8f32601bdf6
Altamash0912 <mohdaltamash128@gmail.com>
2025-10-15 16:26:44 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Display rercords</title>
+    <title>Display Records</title>
 </head>
 <body>
 <table border="3" >
@@ -8,48 +8,40 @@
 <tr>
     <th>S.No.</th>
     <th>Name</th>
-    <th>Student_id</th>
-    <th>Email</th>
+    <th>College Email</th>
+    <th>AFN/Roll No.</th>
     <th>Phone no.</th>
-    <th>Bus route</th>
-    <th>Address</th>
+    <th>Role</th>
+    <th>Bus No.</th>
 </tr>
 
-
 <?php
-error_reporting(0);
-
+error_reporting(E_ALL);
 include("register.php");
 
 $query = "SELECT * FROM students";
 $data = mysqli_query($con, $query);
 $total = mysqli_num_rows($data);
-$result = mysqli_fetch_assoc($data);
 
-if($total !=0)
-{
-    $result = mysqli_fetch_assoc($data);
-    while($result = mysqli_fetch_assoc($data))
-    {
+if($total != 0) {
+    while($result = mysqli_fetch_assoc($data)) {
         echo "
         <tr>
             <td>".$result['id']."</td>
             <td>".$result['name']."</td>
-            <td>".$result['student_id']."</td>
             <td>".$result['email']."</td>
+            <td>".$result['afn_roll_number']."</td>
             <td>".$result['phone']."</td>
-            <td>".$result['route']."</td>
-            <td>".$result['address']."</td>
+            <td>".$result['role']."</td>
+            <td>".$result['bus']."</td>
         </tr>
         ";
     }
-
-}
-else
-{
-    echo "No records found";
+} else {
+    echo "<tr><td colspan='7'>No records found</td></tr>";
 }
 
+mysqli_close($con);
 ?>
 
 </table>
